0dd52428ee148f6592f10d356f5385ff7ccdb48d
William Giammona <wgiammona@gmail.com>
2025-02-08 16:47:52 +0200

    added slug, image, description, and price

--- sanity/schemaTypes/productType.ts
+++ sanity/schemaTypes/productType.ts
@@ -13,6 +13,36 @@ const productType = {
       type: "string",
       validation: (Rule) => Rule.required(),
     }),
+    defineField({
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "name",
+        maxLength: 96,
+      },
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "image",
+      title: "Product Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: "description",
+      title: "Description",
+      type: "blockContent",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "price",
+      title: "Price",
+      type: "string",
+      validation: (Rule) => Rule.required().min(0),
+    }),
   ],
 };
 
