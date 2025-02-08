7dc4320c8fed70d00fbcc3f5e61ee2a6b2589508
William Giammona <wgiammona@gmail.com>
2025-02-08 17:38:41 +0200

    removed post type

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -2,9 +2,8 @@ import { type SchemaTypeDefinition } from "sanity";
 
 import { blockContentType } from "./blockContentType";
 import { categoryType } from "./categoryType";
-import { postType } from "./postType";
 import { productType } from "./productType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [blockContentType, categoryType, postType, productType],
+  types: [blockContentType, categoryType, productType],
 };
