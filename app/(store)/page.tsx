655a24b6fd595626d374c49fdc36b3cb2feedf74
William Giammona <wgiammona@gmail.com>
2025-02-09 13:22:45 +0200

    typegen

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { getAllProducts } from "@/sanity/lib/products/getAllProducts";
 
 export default async function Home() {
   const products = await getAllProducts();
