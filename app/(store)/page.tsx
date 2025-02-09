ea485ec2fcc2bb7ef23d724368eaf60fa706396c
William Giammona <wgiammona@gmail.com>
2025-02-09 12:41:46 +0200

    added getAllProducts function

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+export default async function Home() {
+  const products = await getAllProducts();
   return (
     <div className="">
-      Hello World
+      {/* render all products */}
       <Button>Click Me</Button>
     </div>
   );
