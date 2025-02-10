1cb9e82d7918663575af3c24b4349494e9d7ca4f
William Giammona <wgiammona@gmail.com>
2025-02-10 15:26:22 +0200

    added import and placeholder text for no search result

--- app/(store)/search/page.tsx
+++ app/(store)/search/page.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { searchProductsByName } from "@/sanity/lib/products/searchProductsByName";
+
 const SearchPage = async ({
   searchParams,
 }: {
@@ -8,6 +9,21 @@ const SearchPage = async ({
 }) => {
   const { query } = await searchParams;
   const products = await searchProductsByName(query);
+
+  if (!products.length) {
+    return (
+      <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
+        <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-4xl">
+          <h1 className="text-3xl font-bold mb06 text-center">
+            No products found for: {query}
+          </h1>
+          <p className="text-gray-600 text-center">
+            Try searching with different keywords
+          </p>
+        </div>
+      </div>
+    );
+  }
 
   return <div>Search Page for {query}</div>;
 };
