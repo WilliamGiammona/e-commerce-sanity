ed4ef489d2f9f1a3262c6ca4e0fe93a2437f99f4
William Giammona <wgiammona@gmail.com>
2025-02-13 17:25:34 +0200

    added blue spinner

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Loader() {
-  return <div>Loader</div>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-blue-500"></div>
+    </div>
+  );
 }
 
 export default Loader;
