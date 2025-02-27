f3e885d02cc94f1f1b7f347ef0b529c031f0baa1
William Giammona <wgiammona@gmail.com>
2025-02-27 16:48:48 +0200

    test

--- app/(store)/basket/page.tsx
+++ app/(store)/basket/page.tsx
@@ -1,8 +1,18 @@
 "use client";
-import { useBasketStore } from "../store";
 
+import { useBasketStore } from "../store";
+import { useAuth, useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 function BasketPage() {
   const groupedItems = useBasketStore((state) => state.getGroupedItems());
+  const { isSignedIn } = useAuth();
+  const { user } = useUser();
+  const { router } = useRouter();
+
+  const [isClient, setIsClient] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
   return <div>page</div>;
 }
 
