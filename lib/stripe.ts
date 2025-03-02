3377ebad08b92bd110502003ab0e2681a11b379f
William Giammona <wgiammona@gmail.com>
2025-03-02 20:33:59 +0200

    refactor

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,5 +1,9 @@
 import Stripe from "stripe";
 
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error("Missing Stripe secret key in environment variables");
+}
+
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2025-02-24.acacia",
 });
