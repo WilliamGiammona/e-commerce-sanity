d70ae7800231e1caf0e556aa4409cdf59008270e
William Giammona <wgiammona@gmail.com>
2025-03-02 20:38:45 +0200

    search for existing customers by email

--- actions/createCheckoutSession.ts
+++ actions/createCheckoutSession.ts
@@ -1,4 +1,5 @@
 "use server";
+import stripe from "@/lib/stripe";
 
 import { BasketItem } from "@/app/(store)/store";
 
@@ -23,6 +24,17 @@ export async function createCheckoutSession(
     const itemsWithoutPrice = items.filter((item) => !item.product.price);
     if (itemsWithoutPrice.length > 0) {
       throw new Error("Some items don't have a price.");
+    }
+
+    // Search for existing customer by email
+    const customers = await stripe.customers.list({
+      email: metedata.customerEmail,
+      limit: 1,
+    });
+
+    let customerId: string | undefined;
+    if (customers.data.length > 0) {
+      customerId = customers.data[0].id;
     }
   } catch (error) {
     console.log("Error creating checkout session:", error);
