60c43e8669af1a781d4cdfadd9bbc742f0a9206b
William Giammona <wgiammona@gmail.com>
2025-03-04 14:12:09 +0200

    added imports

--- app/(store)/success/page.tsx
+++ app/(store)/success/page.tsx
@@ -1,1 +1,7 @@
 "use client";
+
+import { useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { useBasketStore } from "../store";
