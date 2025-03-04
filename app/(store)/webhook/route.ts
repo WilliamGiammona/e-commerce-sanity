b6e40d0568925e1cc528fc1f95e19222e09e2b0e
William Giammona <wgiammona@gmail.com>
2025-03-04 15:39:19 +0200

    get text from body, headers, and sig

--- app/(store)/webhook/route.ts
+++ app/(store)/webhook/route.ts
@@ -3,3 +3,9 @@ import { backendClient } from "@/sanity/lib/backendClient";
 import { headers } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 import Stripe from "stripe";
+
+export async function POST(req: NextRequest) {
+  const body = await req.text();
+  const headersList = await headers();
+  const sig = headersList.get("stripe-signature");
+}
