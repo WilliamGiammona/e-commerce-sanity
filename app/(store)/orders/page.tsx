7ef7c39ef6eb5d70d73627b8067b845a6a210f6b
William Giammona <wgiammona@gmail.com>
2025-03-04 17:01:11 +0200

    if no user id, go to home page

--- app/(store)/orders/page.tsx
+++ app/(store)/orders/page.tsx
@@ -1,7 +1,12 @@
 import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 async function Orders() {
   const { userId } = await auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
   return <div>Orders</div>;
 }
 
