a4906c1648df2dde13313db2a81fe36d653cbe7c
William Giammona <wgiammona@gmail.com>
2025-03-04 15:37:23 +0200

    initial commit

---
+++ app/(store)/webhook/route.ts
@@ -0,0 +1,5 @@
+import stripe from "@/lib/stripe";
+import { backendClient } from "@/sanity/lib/backendClient";
+import { headers } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
