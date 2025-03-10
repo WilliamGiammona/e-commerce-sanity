2d47c6bd5da285d867e578f91c0787b0540a1b0b
William Giammona <wgiammona@gmail.com>
2025-03-10 15:30:15 +0200

    refactor

--- components/DisableDraftMode.tsx
+++ components/DisableDraftMode.tsx
@@ -20,8 +20,8 @@ export function DisableDraftMode() {
 
   return (
     <button
-      className="fixed bottom-4 right-4 bg-gray-50 px-4 py-2 z-50"
       onClick={handleClick}
+      className="fixed bottom-4 right-4 bg-gray-50 px-4 py-2 z-50"
     >
       Disable Draft Mode
     </button>
