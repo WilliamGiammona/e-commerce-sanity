ae600072c22b281729f79c65bce2e2f3297d1a19
William Giammona <wgiammona@gmail.com>
2025-03-20 14:19:19 +0200

    disclaimer added

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -2,6 +2,7 @@ import { getAllProducts } from "@/sanity/lib/products/getAllProducts";
 import { getAllCategories } from "@/sanity/lib/products/getAllCategories";
 import ProductsView from "@/components/ProductsView";
 import BlackFridayBanner from "@/components/BlackFridayBanner";
+import Disclaimer from "@/app/disclaimer/page";
 
 export const dynamic = "force-static";
 export const revalidate = 600; // revalidate at most every 60 seconds
@@ -12,6 +13,7 @@ export default async function Home() {
 
   return (
     <div>
+      <Disclaimer />
       <BlackFridayBanner />
       <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
         <ProductsView products={products} categories={categories} />
--- app/disclaimer/page.tsx
+++ app/disclaimer/page.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const Disclaimer = () => {
+  return (
+    <div className="flex justify-center items-center  bg-white p-6">
+      <div className="text-center max-w-4xl">
+        <h1 className="text-4xl md:text-4xl font-bold text-black mb-4">
+          Disclaimer
+        </h1>
+        <p className="text-xl md:text-xl text-black leading-relaxed">
+          This website is for demonstration purposes only. Please only use test
+          payment methods recommended by stripe, real transactions will not be
+          accepted and no products will be delivered
+        </p>
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default Disclaimer;
