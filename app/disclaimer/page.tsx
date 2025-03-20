7e865f2d5acfe88cd0f84435dddd955827597fee
William Giammona <wgiammona@gmail.com>
2025-03-20 14:20:53 +0200

    refactor

--- app/disclaimer/page.tsx
+++ app/disclaimer/page.tsx
@@ -4,10 +4,10 @@ const Disclaimer = () => {
   return (
     <div className="flex justify-center items-center  bg-white p-6">
       <div className="text-center max-w-4xl">
-        <h1 className="text-4xl md:text-4xl font-bold text-black mb-4">
+        <h1 className="text-2xl md:text-4xl font-bold text-black mb-4">
           Disclaimer
         </h1>
-        <p className="text-xl md:text-xl text-black leading-relaxed">
+        <p className="text-lg md:text-xl text-black leading-relaxed">
           This website is for demonstration purposes only. Please only use test
           payment methods recommended by stripe, real transactions will not be
           accepted and no products will be delivered
